6c113fce9a010823d25dbc3b85905fd88d9de6f1
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-08-03 15:52:32 +0530

    Added Instagram & Twitter actual URLs to Socials

--- constants/index.js
+++ constants/index.js
@@ -120,17 +120,17 @@ const socials = [
  {
 	name: "Instagram",
 	icon: "/images/insta.png",
-	url: "#",
+	url: "https://www.instagram.com/coderprithvi/",
  },
  {
 	name: "X (Twitter)",
 	icon: "/images/x.png",
-	url: "#",
+	url: "https://x.com/PrithviPR8",
  },
  {
 	name: "Facebook",
 	icon: "/images/fb.png",
-	url: "#",
+	url: "https://www.instagram.com/coderprithvi/",
  },
 ];
 
